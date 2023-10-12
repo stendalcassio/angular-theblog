3720aa73e1a1f4a300d128de0e683932401d2f49
Stendal <stendalcassio@hotmail.com>
2023-10-12 17:25:23 -0300

    feat: component menu-title e construção dos cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-blog';
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('The Blog');
+  }
 }
