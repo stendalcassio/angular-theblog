7775a8410aa22db8402df01546c912603d88036d
Stendal <stendalcassio@hotmail.com>
2023-10-17 16:49:04 -0300

    finalização do blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Tecnologia Nova no Mercado de Trabalho para Mobile',
+    description: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+    photoCover: 'https://getbots.com.br/wp-content/uploads/2023/05/Blog-noticias-tecnologia-maio-aplicativos.png',
+  },
+  {
+    id: '2',
+    title: 'Nova Série Sobre Serial Killer no Brasil',
+    description: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable.',
+    photoCover: 'https://aventurasnahistoria.uol.com.br/media/_versions/crimes/capa_manchetes_sensasionalistas_widelg.jpg',
+  },
+  {
+    id: '3',
+    title: 'Nova Temporada de One Piece',
+    description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here',
+    photoCover: 'https://sm.ign.com/t/ign_br/tv/o/one-piece-/one-piece-2_1xby.1200.jpg',
+  },
+  {
+    id: '4',
+    title: 'Valorant bate Recordes de Visualizações',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    photoCover: 'https://s2-g1.glbimg.com/KJmfhc3dzVmjSFHjeSTSpm_W9sQ=/0x0:1920x1080/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/d/A/jze8TURRKhTKz9LfLvRA/valorant1.jpg',
+  },
+];
